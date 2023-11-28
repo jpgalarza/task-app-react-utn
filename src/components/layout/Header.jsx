5ad2002fb2f850953b670ff2f14a727d87372aaa
jpgalarza <jpgalarza@hotmail.com.ar>
2023-11-28 15:56:48 -0300

    Se adapta el menú, con CSS y un state, para que sea responsivo

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,29 @@
-
+import { useState } from "react";
+import { IoClose, IoMenu } from "react-icons/io5";
 
 export const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <header>
       <div className="header-container container">
         <span>Task-App</span>
-        <nav>
-          <ul>
-            <li><a href="#">section1</a></li>
-            <li><a href="#">section2</a></li>
-          </ul>
-        </nav>
+        <div className={`menu-container ${openMenu? 'open-menu' : ''}`}>
+          <div className="container-close-menu">
+            <button className="menu-buttons" onClick={() => setOpenMenu(false)}>
+              <IoClose />
+            </button>
+          </div>
+          <nav>
+            <ul>
+              <li><a href="#">section1</a></li>
+              <li><a href="#">section2</a></li>
+            </ul>
+          </nav>
+        </div>
+        <button className="menu-buttons" onClick={() => setOpenMenu(true)}>
+          <IoMenu />
+        </button>
       </div>
     </header>
   )
